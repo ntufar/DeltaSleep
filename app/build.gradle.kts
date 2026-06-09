bb6557b0cb837be8170257612a64c540e603fe9f
Nicolai Tufar <ntufar@gmail.com>
2026-06-10 02:41:14 +0300

    chore: bump to v0.1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.ntufar.deltasleep"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
     }
 
     signingConfigs {
